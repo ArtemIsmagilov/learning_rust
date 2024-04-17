63d746e70edf47cf6890e4870578121f2afaf365
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-04-17 11:20:58 +0300

    used slices

---
+++ slices/src/main.rs
@@ -0,0 +1,54 @@
+fn main() {
+
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    s.clear();
+
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    let slice = &s[0..2];
+    let slice = &s[..2];
+
+    let len = s.len();
+
+    let slice = &s[3..len];
+    let slice = &s[3..];
+
+    let slice = &s[0..len];
+    let slice = &s[..];
+
+    let my_string = s.clone();
+
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    let word = first_word(my_string_literal);
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
