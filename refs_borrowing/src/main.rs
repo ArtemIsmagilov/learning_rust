292b921c694b418a228f0e1f840b28b713eefd86
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-04-17 11:05:14 +0300

    init references and borrowing indicates in Rust

---
+++ refs_borrowing/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+
+    change(&mut s);
+    let len = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, len);
+
+    let mut s = String::from("hello");
+
+    {
+        let _r1 = &mut s;
+    }
+
+    let r2 = &mut s;
+    println!("{}", r2);
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    println!("{}", no_dangle());
+
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}
